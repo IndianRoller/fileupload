b2c50bdb55f1cb2779c618aadf3c0600394d76ee
vsrinivas07 <vsrinivas07@gmail.com>
2016-02-14 21:11:55 +0530

    initial check in

---
+++ src/com/ir/fps1/FileUploadController.java
@@ -0,0 +1,126 @@
+package com.ir.fps1;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+
+/**
+ * Servlet implementation class FileUploadController
+ */
+public class FileUploadController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+   // location to store file uploaded
+    private static final String UPLOAD_DIRECTORY = "upload";
+
+	// upload settings
+    private static final int MEMORY_THRESHOLD   = 1024 * 1024 * 3;  // 3MB
+    private static final int MAX_FILE_SIZE      = 1024 * 1024 * 40; // 40MB
+    private static final int MAX_REQUEST_SIZE   = 1024 * 1024 * 50; // 50MB
+ 
+    
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FileUploadController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// checks if the request actually contains upload file
+        if (!ServletFileUpload.isMultipartContent(request)) {
+            // if not, we stop here
+            PrintWriter writer = response.getWriter();
+            writer.println("Error: Form must has enctype=multipart/form-data.");
+            writer.flush();
+            return;
+        }
+ 
+        // configures upload settings
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        // sets memory threshold - beyond which files are stored in disk
+        factory.setSizeThreshold(MEMORY_THRESHOLD);
+        // sets temporary location to store files
+        factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
+ 
+        ServletFileUpload upload = new ServletFileUpload(factory);
+         
+        // sets maximum size of upload file
+        upload.setFileSizeMax(MAX_FILE_SIZE);
+         
+        // sets maximum size of request (include file + form data)
+        upload.setSizeMax(MAX_REQUEST_SIZE);
+ 
+        // constructs the directory path to store upload file
+        // this path is relative to application's directory
+        String uploadPath = getServletContext().getRealPath("")
+                + File.separator + UPLOAD_DIRECTORY;
+         
+        // creates the directory if it does not exist
+        File uploadDir = new File(uploadPath);
+        if (!uploadDir.exists()) {
+            uploadDir.mkdir();
+        }
+ 
+        try {
+            // parses the request's content to extract file data
+            List<FileItem> formItems = upload.parseRequest(request);
+ 
+            if (formItems != null && formItems.size() > 0) {
+                // iterates over form's fields
+                for (FileItem item : formItems) {
+                    // processes only fields that are not form fields
+                    if (!item.isFormField()) {
+                        String fileName = new File(item.getName()).getName();
+                        String filePath = uploadPath + File.separator + fileName;
+                        File storeFile = new File(filePath);
+
+                        // saves the file on disk
+                        item.write(storeFile);
+                        request.setAttribute("message", "Upload has been done successfully to location"+ filePath);
+            			response.setHeader("Content-disposition", "attachment; filename=\"" + fileName);
+             			response.setHeader("Content-Type","application/"+FilenameUtils.getExtension(filePath));
+             			
+             			
+             			ServletOutputStream sos = null;
+            			try{
+            				sos = response.getOutputStream();
+            				byte[] byteArray = IOUtils.toByteArray(new FileInputStream(storeFile));
+							sos.write(byteArray, 0, byteArray.length);
+            				sos.flush();
+            			}finally{
+            				if(sos != null){
+            					sos.close();
+            				}
+            			}
+                     }
+                }
+            }
+        } catch (Exception ex) {
+            request.setAttribute("message",
+                    "There was an error: " + ex.getMessage());
+        }
+	}
+
+	 
+
+}
